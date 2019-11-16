b9a97a4e2f0fef6bf6fb6b1dc1acdc3c2eb4a7de
peacecheap <57838635+peacecheap@users.noreply.github.com>
2019-11-16 22:37:15 +0700

    add new web update

---
+++ precache-manifest.3bf978eb6eb06e1100845fb9dbaa04d5.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "94cec8ee70b06940879c81b017470a6f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e5b96babfe494980f342",
+    "url": "/static/css/main.9ea1c9a9.chunk.css"
+  },
+  {
+    "revision": "23ac46ff729ea3c72e2d",
+    "url": "/static/js/2.0844a8fd.chunk.js"
+  },
+  {
+    "revision": "e5b96babfe494980f342",
+    "url": "/static/js/main.18b75601.chunk.js"
+  },
+  {
+    "revision": "bec3d17acc6752bb93c1",
+    "url": "/static/js/runtime-main.8631a539.js"
+  },
+  {
+    "revision": "42770fb4afd1cae63fbc33fa41d5eb7b",
+    "url": "/static/media/delivery.42770fb4.png"
+  },
+  {
+    "revision": "2e4e7e638d8a748e69a8bae4882c88d1",
+    "url": "/static/media/jne.2e4e7e63.png"
+  },
+  {
+    "revision": "ac2bb5d7030adc2b4c6aaa3e31191b99",
+    "url": "/static/media/pos.ac2bb5d7.png"
+  },
+  {
+    "revision": "34499dfe752e17fe8cfd796434cd2f6a",
+    "url": "/static/media/tiki.34499dfe.png"
+  }
+]);
